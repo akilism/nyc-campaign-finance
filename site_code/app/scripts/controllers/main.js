6ad4d2b65e8869beebd208371115f9d62f259322
Akil Harris <akil.harris@gmail.com>
2013-12-27 23:30:00 -0500

    flip-wiggle bars (original view)

--- site_code/app/scripts/controllers/main.js
+++ site_code/app/scripts/controllers/main.js
@@ -67,7 +67,7 @@ controllers.controller('OfficeListController', function ($scope, $http) {
             if(data) {
                 var width = 640;
                 var height = 320;
-                var bar_height = 40;
+                var bar_height = 30;
                 var count = data.length;
                 var totalBarHeight = 0;
                 var domainMax = d3.max(data, function(obj) {
@@ -94,49 +94,29 @@ controllers.controller('OfficeListController', function ($scope, $http) {
                 var getColor = function(d, i) {
                     var scaleVal = totalScale(d.total);
                     var r = Math.floor(scaleVal * 0.025);
-                    var g = Math.floor(scaleVal * 0.175);
+                    var g = Math.floor(scaleVal * 0.25);
                     var b = Math.floor(scaleVal  * 0.025);
                     //return 'rgb(' + r + ', ' + g + ', ' + b +')';
-                    return 'rgb(21, 150, 21)';
+                    return 'rgb(16, 160, 16)';
                 };
 
                 var buildWiggleBar = function(d, i) {
 
                     if (!d.total) { return; }
                     var scaleVal = totalScale(d.total);
-                    var getElbows = function() {
-                        if ((d.remainderBarWidth > 0) && (d.totalBars == 1)) {
-                            return 1;
-                        } else if (d.remainderBarWidth > 0) {
-                            return d.totalBars;
-                        } else {
-                            return 0;
-                        }
-                    };
-
                     d.totalBars = Math.floor(scaleVal/width);
                     d.remainderBarWidth = scaleVal % width;
-                    d.elbows = getElbows();
+                    d.elbows = (d.remainderBarWidth > 0) ? d.totalBars : d.totalBars -1;
 //                    console.log('totalBars             :' + d.totalBars);
 //                    console.log('remainderBarWidth     :' + d.remainderBarWidth);
 //                    console.log('elbows                :' + d.elbows);
-                    if (d.remainderBarWidth > 0) {
-                        d3.select(this)
-                            .append('rect')
-                            .attr('x', function () {
-                                if(d.totalBars > 0) {
-                                    return ((d.totalBars % 2) == 0) ? (width - d.remainderBarWidth) - bar_height : bar_height - 2;
-                                }
 
-                                return 0;
-                            })
-                            .attr('y', totalBarHeight)
-                            .style('fill', getColor)
-                            .style('stroke', '#e4eaf0')
-                            .style('stroke-width', '2px')
-                            .attr('height', bar_height)
-                            .attr('width', d.remainderBarWidth);
-                        totalBarHeight += bar_height;
+                    var getX = function(x) {
+                        return ((x % 2) == 0) ? width - bar_height - 2 : bar_height;
+                    };
+
+                    var getY = function(x) {
+                         return (x * bar_height) + totalBarHeight - (bar_height * d.totalBars);
                     }
 
                     for (var x = 0; x < d.totalBars; x++) {
@@ -144,9 +124,9 @@ controllers.controller('OfficeListController', function ($scope, $http) {
                             .append('rect')
                             .attr('x', function () {
                                 if (d.elbows > 0) {
-                                    if (x == d.totalBars - 1) {
-                                        return bar_height - 2;
-                                    } else if (d.remainderBarWidth > 0) {
+                                    if (x == 0) {
+                                        return 0;
+                                    } else if ((x > 0) && (d.remainderBarWidth > 0)) {
                                         return bar_height - 2;
                                     } else {
                                         return 0;
@@ -162,22 +142,43 @@ controllers.controller('OfficeListController', function ($scope, $http) {
                             .attr('height', bar_height)
                             .attr('width', function () {
                                 if (d.elbows > 0) {
-                                    if (x == d.totalBars - 1) {
+                                    if (x == 0) {
                                         return width - bar_height;
-                                    } else if (d.remainderBarWidth > 0) {
+                                    } else if ((x > 0) && (d.remainderBarWidth > 0)) {
                                         return width - (bar_height*2) + 2;
                                     } else {
                                         return width-bar_height;
                                     }
                                 }
-                                return width-2;
+                                return width;
                             });
+
+                        totalBarHeight += bar_height;
+
+                    }
+
+                    if (d.remainderBarWidth > 0) {
+                        d3.select(this)
+                            .append('rect')
+                            .attr('x', function () {
+                                if(d.totalBars > 0) {
+                                    return ((d.totalBars % 2) == 0) ? bar_height - 2 : (width - d.remainderBarWidth) - bar_height;
+                                }
+
+                                return 0;
+                            })
+                            .attr('y', totalBarHeight)
+                            .style('fill', getColor)
+                            .style('stroke', '#e4eaf0')
+                            .style('stroke-width', '2px')
+                            .attr('height', bar_height)
+                            .attr('width', d.remainderBarWidth);
                         totalBarHeight += bar_height;
                     }
 
-                    for (var p = 0; p < d.elbows; p++) {
+                    for (var x = 0; x < d.elbows; x++) {
                         var arc = d3.svg.arc();
-                        if((p % 2) == 1) {
+                        if((x % 2) == 0) {
                             arc.innerRadius(2)
                                 .outerRadius(bar_height-1)
                                 .startAngle(0)
@@ -191,16 +192,8 @@ controllers.controller('OfficeListController', function ($scope, $http) {
                         d3.select(this)
                             .append('path')
                             .attr('d', arc)
-                            .attr('transform', function() {
-                                var getX = function(o) {
-                                    return ((o % 2) == 1) ? width - bar_height - 2 : bar_height;
-                                };
-
-                                var getY = function(o) {
-                                    return (o * bar_height) + totalBarHeight - (bar_height * d.totalBars);
-                                };
-
-                                return 'translate(' + getX(p) + ',' + getY(p) + ')';
+                            .attr('transform', function(d) {
+                                return 'translate(' + getX(x) + ',' + getY(x) + ')';
                             })
                             .style('fill', getColor);
                     }
@@ -211,7 +204,7 @@ controllers.controller('OfficeListController', function ($scope, $http) {
                       .text(d.office + ' - ' + '$' + Number(d.total).toMoney(2))
                       .attr('transform', function(d, i) {
                           if (d.remainderBarWidth > 0) { d.totalBars++; }
-                          return 'translate(' + 15 + ',' + (totalBarHeight - (d.totalBars * bar_height) + (bar_height/2) + 6) + ')';
+                          return 'translate(' + 5 + ',' + (totalBarHeight - (d.totalBars * bar_height) + (bar_height/2) + 6) + ')';
                       });
 
                 };
@@ -230,6 +223,7 @@ controllers.controller('OfficeListController', function ($scope, $http) {
             }
         };
     });
+
 });
 
 controllers.controller('OfficeCandidateListController', function ($scope, $routeParams, $http) {
@@ -373,6 +367,7 @@ controllers.controller('ZipCodeListController', function ($scope, $http) {
 controllers.controller('CandidateDetailsController', function ($scope, $routeParams, $http) {
     $scope.candidateId = $routeParams.candidateId;
     $scope.url = '/api/candidates/' + $scope.candidateId;
+
 
     $http.get('/api/candidates/' + $routeParams.candidateId).success(function(candidate) {
         $scope.candidate = candidate[0];
