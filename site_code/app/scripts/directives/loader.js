dd7cc855dac2a797303677bbcba987a71a8dbb5c
Akil Harris <akil.harris@gmail.com>
2014-03-05 23:57:35 -0500

    added canvas loader

--- site_code/app/scripts/directives/loader.js
+++ site_code/app/scripts/directives/loader.js
@@ -1,21 +1,42 @@
 'use strict';
 
-angular.module('NYCCampFi')
-  .directive('loader', ['$rootScope', function ($rootScope) {
-    return {
-      template: '<div class="loader ping_span">Loading<span>.</span><span>.</span><span>.</span></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        //element.addClass('hide');
+var app = angular.module('NYCCampFi');
+app.directive('loader', ['$rootScope', function ($rootScope) {
+  return {
+    template: '<div id="loader" class="loader spinner"></div>',
+    restrict: 'E',
+    controller: app.Loader,
+    controllerAs: 'loader',
+    link: function postLink(scope, element, attrs) {
+      //element.addClass('hide');
 
-        $rootScope.$on('$routeChangeStart', function () {
-          element.removeClass('hide');
-        });
+      $rootScope.$on('$routeChangeStart', function () {
+        element.removeClass('hide');
+      });
 
-        $rootScope.$on('$NYCCampFiLoaded', function () {
-         element.addClass('hide');
+      $rootScope.$on('$NYCCampFiLoaded', function () {
+        element.addClass('hide');
 
-        })
-      }
-    };
-  }]);
+      })
+    }
+  };
+}]);
+
+
+app.Loader = function ($scope, $element, $attrs) {
+  if(document.getElementById('loader')) {
+    var cl = new CanvasLoader('loader');
+    cl.setColor('#598f08'); // default is '#000000'
+    cl.setShape('rect'); // default is 'oval'
+    cl.setDiameter(88); // default is 40
+    cl.setDensity(77); // default is 40
+    cl.setRange(0.7); // default is 1.3
+    cl.setSpeed(1); // default is 2
+    cl.setFPS(30); // default is 24
+    cl.show(); // Hidden by default
+  }
+};
+
+app.Loader.$inject = ['$scope', '$element', '$attrs'];
+
+
